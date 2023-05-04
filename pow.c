44c024f4fd3f24a1d17b1d0f0f36848cdba7678c
Lucas Brandao <lucasdearaujo.brandao@gmail.com>
2023-05-03 21:39:00 -0300

    update

--- pow.c
+++ pow.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
+int pow_(int base, int exp) {
+    int r = 1;
+    for (int i = 0; i < exp; i++) {
+        r = r * base;
+    }
+    return r;
+}
+
+
 int main() {
     int base;
     printf("digite a base da potencia: ");
     scanf("%d", &base);
-
+    
     int exp;
     printf("digite o expoente: ");
     scanf("%d", &exp);
 
-    int s = 1;
-    for (int i = 0; i < exp; i++) {
-        s = s * base;
-    }
-    printf("%d", s);
+    int r = pow_(base, exp);
+    printf("Potencia = %d\n", r);
     return 0;
 }
